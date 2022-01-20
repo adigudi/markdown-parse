cb43bf09a36f7bcc064ef9a6e842aa810d7b4788
Adi Gudi <agudi24@gmail.com>
2022-01-20 11:44:14 -0800

    added infinite loop test case

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,13 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+            int nextOpenBracket = markdown.indexOf("[");
+            int nextCloseBracket = markdown.indexOf("]");
+            int openParen = markdown.indexOf("(");
+            int closeParen = markdown.indexOf(")");
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
         }
         return toReturn;
     }
