a0aab04ff05605c62996bb0d2cdb183b9bcdae9f
Adi Gudi <agudi24@gmail.com>
2022-02-24 18:52:55 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,14 +12,14 @@ public class MarkdownParseTest {
     public void testSnippet1() throws IOException {
         Path fileName = Path.of("snippet1.md");
         String contents = Files.readString(fileName);
-        assertEquals(List.of("url.com", "google.com"), MarkdownParse.getLinks(contents));
+        assertEquals(List.of("`google.com", "google.com", "ucsd.edu"), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet2() throws IOException {
         Path fileName = Path.of("snippet2.md");
         String contents = Files.readString(fileName);
-        assertEquals(List.of("b.com"), MarkdownParse.getLinks(contents));
+        assertEquals(List.of("a.com", "a.com(())", "example.com"), MarkdownParse.getLinks(contents));
     }
 
     @Test
@@ -27,10 +27,7 @@ public class MarkdownParseTest {
         Path fileName = Path.of("snippet3.md");
         String contents = Files.readString(fileName);
         ArrayList<String> list = new ArrayList<>();
-        list.add("https://www.twitter.com");
-        list.add("https://ucsd-cse15l-w22.github.io/");
-        list.add("https://cse.ucsd.edu/");
-        assertEquals(List.of("url.com", "google.com"), MarkdownParse.getLinks(contents));
+        assertEquals(List.of("https://ucsd-cse15l-w22.github.io/"), MarkdownParse.getLinks(contents));
     }
 
 }
