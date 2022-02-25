162fe27052a0e298b49ecfcd8186aae4211d52a3
Adi Gudi <agudi24@gmail.com>
2022-02-24 18:28:15 -0800

    added snippets

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,99 +1,36 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import org.junit.*;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
-import org.junit.*;
-import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
-    public void testEmpty() throws IOException {
-        assertLinks(List.of(), "testCases/empty.md");
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("snippet1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("url.com", "google.com"), MarkdownParse.getLinks(contents));
     }
 
     @Test
-    public void testExtraSpace() throws IOException {
-        assertLinks(List.of(), "testCases/extraSpace.md");
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("snippet2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("b.com"), MarkdownParse.getLinks(contents));
     }
 
     @Test
-    public void testExtraSpace2() throws IOException {
-        assertLinks(List.of(), "testCases/extraSpace.md");
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("snippet3.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("https://www.twitter.com");
+        list.add("https://ucsd-cse15l-w22.github.io/");
+        list.add("https://cse.ucsd.edu/");
+        assertEquals(List.of("url.com", "google.com"), MarkdownParse.getLinks(contents));
     }
 
-    @Test
-    public void testExtraSpace3() throws IOException {
-        assertLinks(List.of(), "testCases/extraSpace.md");
-    }
-
-    @Test
-    public void testEscape() throws IOException {
-        assertLinks(List.of("https://somethingelse.com"), "testCases/escape.md");
-        System.out.println("change made");
-    }
-
-    @Test
-    public void testJustEscape() throws IOException {
-        assertLinks(List.of(), "testCases/justEscape.md");
-    }
-
-    @Test
-    public void testImage() throws IOException {
-        assertLinks(List.of(), "testCases/image.md");
-    }
-
-    @Test
-    public void testJustBrackets() throws IOException {
-        assertLinks(List.of(), "testCases/justBrackets.md");
-    }
-
-    @Test
-    public void testJustParentheses() throws IOException {
-        assertLinks(List.of(), "testCases/justParentheses.md");
-    }
-
-    @Test
-    public void testMultiline() throws IOException {
-        assertLinks(List.of("https://isthisfound.com"), "testCases/multiline.md");
-    }
-
-    @Test
-    public void testLastLine() throws IOException {
-        assertLinks(List.of("last-line-link-should-be-found"), "testCases/lastLine.md");
-    }
-
-    @Test
-    public void testEndStartParentheses() throws IOException {
-        assertLinks(List.of(), "testCases/endStartParentheses.md");
-    }
-
-    @Test
-    public void testNewline() throws IOException {
-        assertEquals(List.of(),
-                MarkdownParse.getLinks("[link](foo\nbar)"));
-    }
-
-    @Test
-    public void testJoesTests() throws IOException {
-        assertLinks(List.of("https://something.com", "some-page.html"), "testCases/test-file.md");
-        assertLinks(List.of("https://something.com", "some-page.html"), "testCases/test-file2.md");
-        assertLinks(List.of(), "testCases/test-file3.md");
-        assertLinks(List.of(), "testCases/test-file4.md");
-        assertLinks(List.of(), "testCases/test-file5.md");
-        assertLinks(List.of(), "testCases/test-file6.md");
-        assertLinks(List.of(), "testCases/test-file7.md");
-        assertLinks(List.of("a link on the first line"), "testCases/test-file8.md");
-    }
-
-    public static void assertLinks(List<String> expectedLinks, String fileName) throws IOException {
-        Path filePath = Path.of(fileName);
-        String contents = Files.readString(filePath);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-
-        assertEquals(expectedLinks, links);
-    }
 }
